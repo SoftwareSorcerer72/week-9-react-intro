48a44337f69bdf6b9aab0009da82c25531366860
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-15 16:59:55 -0400

    Add styling via our own CSS + Bootstrap.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS
+import './main.css'; // Import our own CSS - Should be imported AFTER bootstrap CSS
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
