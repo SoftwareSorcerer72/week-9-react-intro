00592eb62b931bc80076946e5f9669e616a4634d
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-15 15:36:24 -0400

    Structured app.tsx page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-export default function App() {
+
+export default function App(){
+  const firstName: string = 'Brian';
+  const lastName: string = 'Stanton';
+  const isLoggedIn: boolean = true;
+
+  const posts: {id:number, title:string}[] = [
+      {id: 1, title: 'Happy Monday'},
+      {id: 2, title: 'React Rules!'},
+      {id: 3, title: 'Spring has Sprung'}
+  ]
+
   return (
-    <h1>Hello, world!</h1>
-  );
+      <>
+          <h1>Hello World</h1>
+          <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
+          {posts.map( p => <h4>{p.title}</h4> )}
+      </>
+  )
 }
-   
-  
