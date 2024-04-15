8b8c90c4de754a3681dece735d0f640d5f29efd1
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-15 16:12:02 -0400

    Add Navigation componet with isLoggedIn prop.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
+import Navigation from './components/Navigation';
+
 
 export default function App(){
-  const firstName: string = 'Brian';
-  const lastName: string = 'Stanton';
-  const isLoggedIn: boolean = true;
+    const firstName: string = 'Brian';
+    const lastName: string = 'Stanton';
+    const isLoggedIn: boolean = true;
 
-  const posts: {id:number, title:string}[] = [
-      {id: 1, title: 'Happy Monday'},
-      {id: 2, title: 'React Rules!'},
-      {id: 3, title: 'Spring has Sprung'}
-  ]
+    const posts: {id:number, title:string}[] = [
+        {id: 1, title: 'Happy Monday'},
+        {id: 2, title: 'React Rules!'},
+        {id: 3, title: 'Spring has Sprung'}
+    ]
 
-  return (
-      <>
-          <h1>Hello World</h1>
-          <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
-          {posts.map( p => <h4>{p.title}</h4> )}
-      </>
-  )
+    return (
+        <>
+            <Navigation isLoggedIn={isLoggedIn}/>
+            <h1>Hello World</h1>
+            <h2>{isLoggedIn ? `Welcome Back ${firstName} ${lastName}` : 'Please Log In or Sign Up'}</h2>
+            {posts.map( p => <h4 key={p.id}>{p.title}</h4> )}
+        </>
+    )
 }
