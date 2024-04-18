370c1fe28f3fe8b110537a241f2f5d47b3c6c6f5
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-18 17:33:22 -0400

    Set up post card button for editing and connected add post to database.

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -9,10 +9,11 @@ import { useNavigate } from 'react-router-dom';
 
 
 type LoginProps = {
-    flashMessage: (newMessage:string|undefined, newCategory:CategoryType|undefined) => void
+    flashMessage: (newMessage:string|undefined, newCategory:CategoryType|undefined) => void,
+    logUserIn: () => void,
 }
 
-export default function Login({ flashMessage }: LoginProps) {
+export default function Login({ flashMessage, logUserIn }: LoginProps) {
     const navigate = useNavigate();
 
     const [userFormData, setUserFormData] = useState<Partial<UserFormDataType>>(
@@ -35,6 +36,11 @@ export default function Login({ flashMessage }: LoginProps) {
         if (response.error){
             flashMessage(response.error, 'danger')
         } else {
+            const token = response.data!.token;
+            const tokenExp = response.data!.tokenExpiration;
+            localStorage.setItem('token', token);
+            localStorage.setItem('tokenExp', tokenExp);
+            logUserIn();
             flashMessage(response.data?.token, 'success');
             navigate('/')
         }
